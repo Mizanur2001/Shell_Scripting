79164c2d43775de441329a236619d97e6459e134
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-11-25 17:25:56 +0530

    add basic calculator program

---
+++ 1_calculator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#---------------------------------------
+#Author: Masyud Hossain
+#Date: 25/11/2025
+#Task: Basic Calculator
+#---------------------------------------
+
+a=4
+b=2
+
+echo "Number are : $a, $b"
+
+echo "Addtion: $((a+b))"
+echo "Subtraction: $((a-b))"
+echo "Multiplication: $((a*b))"
+echo "Division: $((a/b))"
