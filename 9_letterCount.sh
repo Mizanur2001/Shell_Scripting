acf831f6bf3d675ebaffbb0b9738f42d52f73910
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-11-29 21:28:39 +0530

    add letterCount

---
+++ 9_letterCount.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#-------------------------------------------------------
+#Author: Masyud Hossain
+#Date: 29/11/2025
+#Task: Count and print the number of letters in a words"
+#--------------------------------------------------------
+
+word="Masyud"
+
+count=${#word}
+
+echo "Number of letter in the $word is $count"
