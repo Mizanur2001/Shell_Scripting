35418204cde0fcd655005516a449d469d6a5787a
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-12-30 18:53:05 +0530

    Add file systemUsageInfo

---
+++ 12_systemUsageInfo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#----------------------------------------
+#Author: Masyud Hossain
+#Date:30/12/2025
+#Task: System usage script
+#----------------------------------------
+
+
+echo "-------------------------------------"
+echo "System Usage Report"
+echo "Date: $(date)"
+echo "Hostname: $(hostname)"
+echo "-------------------------------------"
+
+###------------------Cpu Usage-----------------###
+Cpu_Usage=$(top -bn1 | grep "Cpu(s)" | awk '{for(i=1;i<=NF;i++){if($i ~ /id,?$/){print 100 - $(i-1)}}}')
+Cpu_Usage_Int=${Cpu_Usage%.*}
+
+echo "Cpu Usage: ${Cpu_Usage_Int}%"
+
+###------------------Ram Usage----------------###
+Total_Mem=$(free -m | awk '/Mem:/ {print $2}')
+Used_Mem=$(free -m | awk '/Mem:/ {print $3}')
+Ram_Usage_Percent=$(( Used_Mem * 100 / Total_Mem ))
+
+echo "RAM Usage: ${Used_Mem}MB / ${Total_Mem}MB (${Ram_Usage_Percent}%)"
+
